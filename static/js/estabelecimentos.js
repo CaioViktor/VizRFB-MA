afdbb0dc86a7870c48f5f3273436425a4cb54427
CaioViktor <caioviktor@hotmail.com>
2021-07-09 17:43:49 -0300

    visualizaçoes

--- static/js/estabelecimentos.js
+++ static/js/estabelecimentos.js
@@ -1,6 +1,10 @@
 // const  path_estabelecimentos = path_data+"estabelecimentos.csv";
 
-const  path_estabelecimentos = path_data+"estabelecimentos_min.csv";
+// const  path_estabelecimentos = path_data+"estabelecimentos_min.csv";
+const  path_mapa = path_data+"mapa.csv";
+const  path_estabelecimentos_abertos = path_data+"inicio_estabs_formatado.csv";
+const  path_porte_situacao = path_data+"situacao_porte.csv";
+const  path_situacao = path_data+"situacao_formatado.csv";
 const  path_brazil = path_data+"maranhao_pop.json";
 var w = window.innerWidth;
 var h = window.innerHeight;
@@ -15,10 +19,20 @@ let format = d3.format(".2f");
 let facts = null;
 let brazil = null;
 let dim_states = null;
-let group_states = null;
+let group_municipios = null;
 let previous_filter = null;
 let previous_this_map = null;
 let resetar = null;
+let parseDate = d3.utcParse("%Y-%m-%d");
+let colorScale = d3.scaleOrdinal()
+					.domain(["Ativa","Baixada","Inapta","Nula","Suspensa"])
+					.range(["#8dd3c7", "#fb8072", "#e0e004","#80b1d3","#bebada"])
+tratar_dados = function(d){
+	d.date_month = parseDate(d.data.substr(0,7)+"-02");
+	d.date_month_str = d.data.substr(0,7)+"-02";
+	d.data = parseDate(d.data);
+	d.QTD = + d.QTD;
+}
 
 function clickFilter(el){
 	var df = $("#d"+el.id)[0];
@@ -58,8 +72,12 @@ function renderMap(data,maps){
 	.selectAll("path")
 	  .data(data.features)
 	.enter().append("path")
-	//   .attr("fill", d => colorScale_mapa(maps[1].get(d.id)))
-	  .attr("fill", "blue")
+	  .attr("fill",function(d){
+			if(maps[1].has(d.properties.id_rfb))
+				return colorScale_mapa(maps[1].get(d.properties.id_rfb));
+			else
+				return "white";
+		})
 	  .attr("d", path)
 	  .attr("element_id",d=>d.properties.id_rfb)
 	  .on("click",function(d){
@@ -130,11 +148,11 @@ function renderMap(data,maps){
 	return svg.node()
 }
 
-function update_countStates(group){
+function update_countCities(group){
 	let map = new Map();
 	let map_100000 = new Map();
-	let states = group.all();
-	states.forEach(function(d){
+	let cities = group.all();
+	cities.forEach(function(d){
 		map.set(d.key,+d.value);
 		const population = city_population.get(d.key);
 		const to_100000 = (d.value * 100000)/population;
@@ -180,10 +198,17 @@ function showTooltip(maps,element_id, x, y) {
 	const offset = 10;
 	const t = d3.select("#tooltip");
 	
-	t.select("#qtd").text(maps[0].get(element_id));
-	t.select("#pop").text(city_population.get(element_id));
-	t.select("#taxa").text(maps[1].get(element_id));
+	if(maps[0].has(element_id)){
+		t.select("#qtd").text(maps[0].get(element_id));
+		t.select("#taxa").text(maps[1].get(element_id));
+		t.select("#name_county").text(city_name_mapa.get(element_id));
+	}
+	else{
+		t.select("#qtd").text("0");
+		t.select("#taxa").text("0");
+	}
 	t.select("#name_county").text(city_name_mapa.get(element_id));
+	t.select("#pop").text(city_population.get(element_id));
 	t.classed("hidden", false);
 	const rect = t.node().getBoundingClientRect();
 	const wi = rect.width;
@@ -206,8 +231,8 @@ function hideTooltip(map){
 function createMap(){
 	$("#Q1").empty();
 	
-	const maps = update_countStates(group_states);
-	// const domain = [group_states.top(group_states.size())[group_states.size()-1].value,group_states.top(1)[0].value];
+	const maps = update_countCities(group_municipios);
+	// const domain = [group_municipios.top(group_municipios.size())[group_municipios.size()-1].value,group_municipios.top(1)[0].value];
 	//aqui
 	let min= Number.MAX_VALUE, max = Number.MIN_VALUE;
 	maps[1].forEach(function(d){
@@ -216,7 +241,7 @@ function createMap(){
 		if(d < min)
 			min = d;
 	});
-	const domain = [min,max];
+	const domain = [0,max];
 	//aqui
 	const color_scheme = d3.schemeGreens[7];
 	const values_variation = domain[1] - domain[0];
@@ -227,15 +252,15 @@ function createMap(){
 	createLegend(color_scheme);
 }
 
-var data = d3.csv(path_estabelecimentos).then(function(data){
-	//root_cnpj,cnpj_est,name,date_start,situation,situationDate,state,porte,activity
-	let parseDate = d3.utcParse("%Y-%m-%d");
-	data.forEach(function(d){
-		d.date_start_month = parseDate(d.date_start.substr(0,7)+"-02");
-		d.date_start = parseDate(d.date_start);
-		d.situationDate_month = d.situationDate.substr(0,7)+"-01";
-		d.situationDate = parseDate(d.situationDate);
-	});
+// var data = d3.csv(path_estabelecimentos).then(function(data){
+// 	//root_cnpj,cnpj_est,name,date_start,situation,situationDate,state,porte,activity
+// 	let parseDate = d3.utcParse("%Y-%m-%d");
+// 	data.forEach(function(d){
+// 		d.date_month = parseDate(d.date_start.substr(0,7)+"-02");
+// 		d.date_start = parseDate(d.date_start);
+// 		d.date_month = d.situationDate.substr(0,7)+"-01";
+// 		d.situationDate = parseDate(d.situationDate);
+// 	});
 	
 	// let lineChartQ11 = dc.lineChart("#Q11");
 	// let data_table = dc.dataTable("#table_estabs");
@@ -260,14 +285,15 @@ var data = d3.csv(path_estabelecimentos).then(function(data){
 	// 	dc.renderAll();
 	// };
 
-
-	facts = crossfilter(data);
-
-
+var data_mapa = d3.csv(path_mapa).then(function(data){
+	data.forEach(function(d){
+		d.QTD = +d.QTD;
+	});
+	let facts = crossfilter(data);//TODO: No futuro interligar gráficos
 	// //Map dim e group
-	dim_states = facts.dimension(d => d.state);
-	group_states = dim_states.group();
-
+	dim_municipios = facts.dimension(d => d.MUNICIPIO);
+	group_municipios = dim_municipios.group().reduceSum(d=>d.QTD);
+	
 	//Q1
 	d3.json(path_brazil).then(function(data2){
 		data2.features.forEach(function(d){
@@ -277,45 +303,58 @@ var data = d3.csv(path_estabelecimentos).then(function(data){
 		brazil = data2;
 		createMap();
 	});
+	
+	return data;
+})
+	
 
 
-	// //Q11
-	// let dim_date_start = facts.dimension(d=>d.date_start_month);
-	// let _group = dim_date_start.group().reduceSum(function(d){return 1});
-	// var group_date_start = {
-	// 	all:function () {
-	// 		var cumulate = 0;
-	// 		var g = [];
-	// 		_group.all().forEach(function(d,i) {
-	// 			cumulate += d.value;
-	// 			g.push({key:d.key,value:cumulate,'in_day':d.value})
-	// 		});
-	// 		return g;
-	// 	}
-	// };
-	// let hourScale = d3.scaleTime().domain(d3.extent(data,d=> d.date_start_month))
+	
+Q11
+let lineChartQ11 = dc.lineChart("#Q11");
+var data_estabelecimentos_abertos = d3.csv(path_estabelecimentos_abertos).then(function(data){
+	data.forEach(tratar_dados);
+	
+	let facts = crossfilter(data);//TODO: No futuro interligar gráficos
 
-	// lineChartQ11.width(colw)
-    //         .height(colh/2)
- 	// 			.margins({left: 50, top: 5, right: 5, bottom: 30})
-    //         .dimension(dim_date_start)
-    //         .group(group_date_start)
-    //         .x(hourScale)
-    //         .renderArea(true)
-    //         .yAxisLabel("Qtd. de estabelecimentos")
-	//         .xAxisLabel("Ano de início das atividades")
-	//         .clipPadding(10)
-	//         .elasticY(true)
-	//         .title(function(d) { return 'Mês: ' + d.key.toLocaleDateString().substr(3)+'\nAcumulado: '+ d.value+'\nNo mês: '+d.in_day; })
-    //         .brushOn(false)
-    //         .mouseZoomable(true)
-    //         .on("filtered", function(chart,filter){
-	// 		        createMap()
-	// 		});
+	let dim_date_start = facts.dimension(d=>d.date_month);
+	let _group = dim_date_start.group().reduceSum(function(d){return d.QTD});
+	var group_date_start = {
+		all:function () {
+			var cumulate = 0;
+			var g = [];
+			_group.all().forEach(function(d,i) {
+				cumulate += d.value;
+				g.push({key:d.key,value:cumulate,'in_day':d.value})
+			});
+			return g;
+		}
+	};
+	let hourScale = d3.scaleTime().domain(d3.extent(data,d=> d.date_month));
+	lineChartQ11.width(colw)
+			.height(colh/2)
+			.margins({left: 50, top: 5, right: 5, bottom: 30})
+			.dimension(dim_date_start)
+			.group(group_date_start)
+			.x(hourScale)
+			.renderArea(true)
+			.yAxisLabel("Qtd. de estabelecimentos")
+			.xAxisLabel("Ano de início das atividades")
+			.clipPadding(10)
+			.elasticY(true)
+			.title(function(d) { return 'Mês: ' + d.key.toLocaleDateString().substr(3)+'\nAcumulado: '+ d.value+'\nNo mês: '+d.in_day; })
+			.brushOn(false)
+			.mouseZoomable(true)
+			.on("filtered", function(chart,filter){
+					createMap()
+			});
+	lineChartQ11.render();
+	return data;
+})
 
 
 
-    // let dim_date_start_filter = facts.dimension(d=>d.date_start_month);
+    // let dim_date_start_filter = facts.dimension(d=>d.date_month);
     // let group_date_start_filter = dim_date_start.group();
 	// lineChartQ11Filter.width(colw/2)
     //         .height(colh/3)
@@ -341,134 +380,82 @@ var data = d3.csv(path_estabelecimentos).then(function(data){
 
 
 
-    // //Q3
-    // let dim_porte = facts.dimension(function(d){ return d.porte});
-    // let situacoes = ["Ativa","Baixada","Inapta","Nula","Suspensa"];
-    // barchart.ordering(function(d){
-    // 	let cont = 0;
-    // 	for(i in d.value) {
-    // 		cont+= d.value[i];
-    // 	}
-    // 	return -cont;
-    // });
-
-    // let group_porte_situacao = dim_porte.group().reduce(function(p,v){
-	// 		//Add
-	// 		p[v.situation] = (p[v.situation] || 0) + 1;
-	// 		return p;
-	// 	},function(p,v){
-	// 		//Remove
-	// 		p[v.situation] = (p[v.situation] || 0) - 1;
-	// 		return p;
-	// 	},function(p,v){
-	// 		//Init
-	// 		return {};
-	// 	});
+//Q3
+let barchart= dc.barChart("#Q3");
+let data_situacao_porte = d3.csv(path_porte_situacao).then(function(data){
+	data.forEach(function(d){
+		situacoes_map = {"ACTIVE":"Ativa","DOWN":"Baixada","UNFIT":"Inapta","NULL":"Nula","SUSPENDED":"Suspensa"}
+		d.URI_CLASSE_TIPO_SITUACAO = situacoes_map[d.URI_CLASSE_TIPO_SITUACAO];
+		d.QTD = +d.QTD;
+	});
+	let facts = crossfilter(data);//TODO: No futuro interligar gráficos
+	let dim_porte = facts.dimension(function(d){ return d.URI_PORTE});
+	let situacoes = ["Ativa","Baixada","Inapta","Nula","Suspensa"];
+	barchart.ordering(function(d){
+		let cont = 0;
+		for(i in d.value) {
+			cont+= d.value[i];
+		}
+		return -cont;
+	});
+	
+	let group_porte_situacao = dim_porte.group().reduce(function(p,v){
+			//Add
+			p[v.URI_CLASSE_TIPO_SITUACAO] = (p[v.URI_CLASSE_TIPO_SITUACAO] || 0) + v.QTD;
+			return p;
+		},function(p,v){
+			//Remove
+			p[v.URI_CLASSE_TIPO_SITUACAO] = (p[v.URI_CLASSE_TIPO_SITUACAO] || 0) - v.QTD;
+			return p;
+		},function(p,v){
+			//Init
+			return {};
+		});
 		
-	// 	function sel_stack(i) {
-    //           return function(d) {
-    //             	return d.value[i];
-    //           };
-    //       	}
+		function sel_stack(i) {
+				return function(d) {
+					return d.value[i];
+				};
+			}
+	console.log(group_porte_situacao.all())
+		
+	
+	barchart.width(colw)
+				.height(colh)
+				.x(d3.scaleLinear().domain([1, 21]))
+				.margins({left: 80, top: 20, right: 10, bottom: 40})
+				.brushOn(false)
+				.clipPadding(20)
+				.gap(40)
+				.yAxisLabel("Qtd. de estabelecimentos")
+				.xAxisLabel("Porte")
+				.elasticY(true)
+				.title(function (d) {
+					let total = 0
+					for (i in d.value){
+						total += d.value[i];
+					}
+					let rate = format((d.value[this.layer]/ total) * 100);
+					return d.key + '[' + this.layer + ']: ' + d.value[this.layer]+"\n"+rate+"%";
+				})
+				.x(d3.scaleBand())
+				.xUnits(dc.units.ordinal)
+				.dimension(dim_porte)
+				.group(group_porte_situacao, situacoes[0], sel_stack(situacoes[0]))
+				.renderLabel(true)
+				.colors(colorScale)
+				.on("filtered", function(chart,filter){
+					createMap()
+				});
+	
+	barchart.legend(dc.legend());
+	for (var i = 1; i < 5; ++i) {
+				barchart.stack(group_porte_situacao, '' + situacoes[i], sel_stack(situacoes[i]));
+	}
+	barchart.render();
+	return data;
+});
 
-    // let colorScale = d3.scaleOrdinal()
-    //              .domain(["Ativa","Baixada","Inapta","Nula","Suspensa"])
-    //              .range(["#8dd3c7", "#fb8072", "#e0e004","#80b1d3","#bebada"])
-
-   
- 
-    // barchart.width(colw)
-    //             .height(colh)
-    //             .x(d3.scaleLinear().domain([1, 21]))
-    //             .margins({left: 80, top: 20, right: 10, bottom: 40})
-    //             .brushOn(false)
-    //             .clipPadding(20)
-    //             .gap(40)
-    //             .yAxisLabel("Qtd. de estabelecimentos")
-	//         	.xAxisLabel("Porte")
-	//         	.elasticY(true)
-    //             .title(function (d) {
-    //             	let total = 0
-    //             	for (i in d.value){
-    //             		total += d.value[i];
-    //             	}
-    //             	let rate = format((d.value[this.layer]/ total) * 100);
-    //                 return d.key + '[' + this.layer + ']: ' + d.value[this.layer]+"\n"+rate+"%";
-    //             })
-    //             .x(d3.scaleBand())
-    //         	.xUnits(dc.units.ordinal)
-    //             .dimension(dim_porte)
-    //             .group(group_porte_situacao, situacoes[0], sel_stack(situacoes[0]))
-    //             .renderLabel(true)
-    //             .colors(colorScale)
-    //             .on("filtered", function(chart,filter){
-	// 		        createMap()
-	// 		    });
-
-    // barchart.legend(dc.legend());
-    // for (var i = 1; i < 5; ++i) {
-    //             barchart.stack(group_porte_situacao, '' + situacoes[i], sel_stack(situacoes[i]));
-	// }
-
-
-    // //Datatable
-    // let dim_cnpj = facts.dimension(d=>d.cnpj_est);
-    // var ofs = 0, pag = 100;
-
-	// function update_offset() {
-	//   var totFilteredRecs = facts.groupAll().value();
-
-	//   var end = ofs + pag > totFilteredRecs ? totFilteredRecs : ofs + pag;
-	//   ofs = ofs >= totFilteredRecs ? Math.floor((totFilteredRecs - 1) / pag) * pag : ofs;
-	//   ofs = ofs < 0 ? 0 : ofs;
-
-
-	//   data_table.beginSlice(ofs);
-	//   data_table.endSlice(ofs+pag);
-	// }
-
-	// function display() {
-	//   var totFilteredRecs = facts.groupAll().value();
-	//   var end = ofs + pag > totFilteredRecs ? totFilteredRecs : ofs + pag;
-	//   d3.select('#begin')
-	//       .text(end === 0? ofs : ofs + 1);
-	//   d3.select('#end')
-	//       .text(end);
-	//   d3.select('#last')
-	//       .attr('disabled', ofs-pag<0 ? 'true' : null);
-	//   d3.select('#next')
-	//       .attr('disabled', ofs+pag>=totFilteredRecs ? 'true' : null);
-	//   d3.select('#size').text(totFilteredRecs);
-	//   if(totFilteredRecs != facts.size()){
-	//     d3.select('#totalsize').text("(filtered Total: " + facts.size() + " )");
-	//   }else{
-	//     d3.select('#totalsize').text('');
-	//   }
-
-	// }
-
-	// next=function(){
-	// 	ofs += pag;
-	// 	update_offset();
-	// 	data_table.redraw();
-	// }
-
-	// last=function(){
-	// 	ofs -= pag;
-	// 	update_offset();
-	// 	data_table.redraw();
-	// }
-
-    // data_table.width(w)
-    //         .height(h)
-    //         .dimension(dim_cnpj)
-    //         .size(Infinity)
-    //         .columns([d=>"<a title='Ver rede de sócios' target='_blank' href='rede_socios.html?cnpj="+d.root_cnpj+"'>"+d.root_cnpj+"</a>",d=>d.cnpj_est,d=>d.name,d=>d.porte,d=>d.date_start.toLocaleDateString(),d=>d.situation,d=>d.situationDate.toLocaleDateString(),d=>d.state,d=>d.activity])
-    //         .sortBy(d=>d.root_cnpj)
-    //         .order(d3.ascending)
-    //         .on('preRender', update_offset)
-    //       	.on('preRedraw', update_offset)
-    //       	.on('pretransition', display);
 
 
 
@@ -476,57 +463,62 @@ var data = d3.csv(path_estabelecimentos).then(function(data){
 
     
            
-    // //Gráfico de linha das situações
-	// let dim_situation = facts.dimension(d=>[d.situation,d.situationDate_month]);
-	// let _group_situation = dim_situation.group();
+//Gráfico de linha das situações
+let lineChart_situation = dc.seriesChart("#situacoes_linha");
+let data_situacao = d3.csv(path_situacao).then(function(data){
+	data.forEach(tratar_dados);
+	let facts = crossfilter(data);//TODO: No futuro interligar gráficos
+	let dim_situation = facts.dimension(d=>[d.URI_CLASSE_TIPO_SITUACAO,d.date_month_str]);
+	let _group_situation = dim_situation.group();
 	
-	// var group_situation = {
-	// 	all:function () {
-	// 		var cumulate = {};
-	// 		var g = [];
-	// 		let parseDate = d3.utcParse("%Y-%m-%d");
-	// 		_group_situation.all().forEach(function(d,i) {
-	// 			if(!(d.key[0] in cumulate))
-	// 				cumulate[d.key[0]] = d.value;
-	// 			else
-	// 				cumulate[d.key[0]] += d.value;
-	// 			g.push({key:[d.key[0],parseDate(d.key[1])],value:cumulate[d.key[0]],'in_day':d.value})
-	// 		});
+	var group_situation = {
+		all:function () {
+			var cumulate = {};
+			var g = [];
+			_group_situation.all().forEach(function(d,i) {
+				if(!(d.key[0] in cumulate))
+				cumulate[d.key[0]] = d.value;
+				else
+				cumulate[d.key[0]] += d.value;
+				g.push({key:[d.key[0],parseDate(d.key[1])],value:cumulate[d.key[0]],'in_day':d.value})
+			});
 			
-		
-	// 		return g;
-	// 	}
-	// };
-	// let hourScale_situation = d3.scaleTime().domain(d3.extent(data,function(d){
-	// 	let parseDate = d3.utcParse("%Y-%m-%d");
-	// 	return parseDate(d.situationDate_month)
-	// }))
+			
+			return g;
+		}
+	};
+	let hourScale_situation = d3.scaleTime().domain(d3.extent(data,function(d){
+		return parseDate(d.date_month_str)
+	}))
+	lineChart_situation.width(colw)
+				     .height(colh/2)
+				     .margins({left: 50, top: 5, right: 5, bottom: 30})
+				     .chart(function(c) { return new dc.LineChart(c); })
+				     .x(hourScale_situation)
+				     .brushOn(false)
+				     .yAxisLabel("Qtd. de estabelecimentos")
+				     .xAxisLabel("Data da situação")
+				     .clipPadding(10)
+				     .elasticY(true)
+				     .dimension(dim_situation)
+				     .group(group_situation)
+				     .mouseZoomable(false)
+				     .title(function(d) { return 'Situação: '+d.key[0]+'\nMês: ' + d.key[1].toLocaleDateString().substr(3)+'\nAcumulado: '+ d.value+'\nNo mês: '+d.in_day; })
+				     .seriesAccessor(function(d) { return d.key[0];})
+				     .keyAccessor(function(d) {return d.key[1];})
+				     .valueAccessor(function(d) { return +d.value;})
+				     .colors(colorScale)
+				     .on("filtered", function(chart,filter){
+					    createMap();
+					})
+				     .legend(dc.legend().x(250).y(0).itemHeight(13).gap(5))
+				     .xAxis().ticks(5);
+	lineChart_situation.render();
+	return data;
+})
 
 
 
-	// lineChart_situation.width(colw)
-	// 			     .height(colh/2)
-	// 			     .margins({left: 50, top: 5, right: 5, bottom: 30})
-	// 			     .chart(function(c) { return new dc.LineChart(c); })
-	// 			     .x(hourScale_situation)
-	// 			     .brushOn(false)
-	// 			     .yAxisLabel("Qtd. de estabelecimentos")
-	// 			     .xAxisLabel("Data da situação")
-	// 			     .clipPadding(10)
-	// 			     .elasticY(true)
-	// 			     .dimension(dim_situation)
-	// 			     .group(group_situation)
-	// 			     .mouseZoomable(false)
-	// 			     .title(function(d) { return 'Situação: '+d.key[0]+'\nMês: ' + d.key[1].toLocaleDateString().substr(3)+'\nAcumulado: '+ d.value+'\nNo mês: '+d.in_day; })
-	// 			     .seriesAccessor(function(d) { return d.key[0];})
-	// 			     .keyAccessor(function(d) {return d.key[1];})
-	// 			     .valueAccessor(function(d) { return +d.value;})
-	// 			     .colors(colorScale)
-	// 			     .on("filtered", function(chart,filter){
-	// 				    createMap();
-	// 				})
-	// 			     .legend(dc.legend().x(250).y(0).itemHeight(13).gap(5))
-	// 			     .xAxis().ticks(5);
 
 
 
@@ -588,4 +580,4 @@ var data = d3.csv(path_estabelecimentos).then(function(data){
 // 	}
 // AddXAxis(rowChartQ2, "Qtd. de estabelecimentos");
 //   return data;
-});
+// });
